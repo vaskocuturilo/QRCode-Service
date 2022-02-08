41f728968516fa32983b0bf708859fa97d5f1268
vaskocuturilo <vaskocuturilo@rambler.ru>
2022-02-08 22:44:24 +0300

    DV-0001: Add Project.

---
+++ src/main/java/site/engineertest/qrcodeservice/QrcodeServiceApplication.java
@@ -0,0 +1,13 @@
+package site.engineertest.qrcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/engineertest/qrcodeservice/QrcodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.engineertest.qrcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
